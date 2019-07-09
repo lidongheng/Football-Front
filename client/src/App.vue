cf2b25ca7faea16be91ae8eab11661bdbcf41d27
lidongheng <huoyi-20@163.com>
2019-07-10 01:47:45 +0800

    把Main组件更名为Home组件

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,13 +7,11 @@
 </template>
 
 <script>
-import Main from './components/Main/Main'
 import Nav from './components/Nav/Nav'
 import Footer from './components/Footer/Footer'
 export default {
   name: 'App',
   components: {
-    Main,
     'app-nav': Nav,
     'app-footer': Footer
   }
