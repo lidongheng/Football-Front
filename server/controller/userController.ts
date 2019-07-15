5e71255d0681fac76175ce61768eb2fb0397f316
lidongheng <huoyi-20@163.com>
2019-07-15 18:55:24 +0800

    加入vaidator类，并用在注册和登录模块上

--- server/controller/userController.ts
+++ server/controller/userController.ts
@@ -4,15 +4,21 @@ import bcrypt from "bcrypt";
 import * as jwt from 'jsonwebtoken';
 import SecretOrKey from '../config/secret';
 import { UserModel } from '../models/Users';
+import { Validator } from '../validation/validator';
 
 
 class UserController {
 
     public async register(req: Request, res: Response) {
+        const {errors, isValid } = Validator.registerInput(req.body);
+        if(!isValid){
+            return res.status(400).json(errors);
+        }
         //查询数据库是否拥有邮箱
         const user = await UserModel.findOne({email: req.body.email});
         if (user) {
             res.status(400).json({email: "邮箱已被注册"});
+            return ;
         }
         const saltRounds = 10;
         req.body.password = await bcrypt.hash(req.body.password, saltRounds);
@@ -32,15 +38,17 @@ class UserController {
         });
     }
     public async login(req: Request, res: Response) {
-        const email = req.body.email;
-        const password = req.body.password;
-        const user = await UserModel.findOne({email})
+        const {errors, isValid } = Validator.loginInput(req.body);
+        if(!isValid){
+            return res.status(400).json(errors);
+        }
+        const user = await UserModel.findOne({email:req.body.email})
         if(!user) {
             res.status(404).json({"message": "用户不存在"});
         }
         // 密码匹配
         // @ts-ignore
-        const match = await bcrypt.compare(password, user.password)
+        const match = await bcrypt.compare(req.body.password, user.password)
         if(match) {
             // @ts-ignore
             const rule = {id:user.id,email:user.email};
